330d483ec80df884f4bbd4d74345eee894293564
Anrighe <assoonpc@gmail.com>
2023-05-15 15:45:42 +0200

    small change

--- F1/media/models.py
+++ F1/media/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class PortaleF1(models.Model):
     titolo = models.CharField(max_length=100)
 
@@ -10,8 +11,16 @@ class News(models.Model):
     contenuto = models.CharField(max_length=500)
     autore = models.CharField(max_length=50)
     tags = models.CharField(max_length=250)  # tag separati dal carattere ,
+
+class Highlight(models.Model):
+    titolo = models.CharField(max_length=100)
+    video = models.CharField(max_length=100)
+    visualizzazioni = models.PositiveIntegerField()
     
 
 News.contiene = models.OneToOneField(PortaleF1, on_delete=models.PROTECT)
-PortaleF1.contiene = models.ManyToManyField(News, blank=True)
+PortaleF1.contiene_news = models.ManyToManyField(News, blank=True)
+Highlight.contiene = models.OneToOneField(PortaleF1, on_delete=models.PROTECT)
+PortaleF1.contiene_highlight = models.ManyToManyField(Highlight, blank=True)
+
 
