d4da34d07730917d59ae48f0fefdbef95194d2b3
Anrighe <assoonpc@gmail.com>
2023-05-23 17:15:29 +0200

    fixed wallpaper

---
+++ F1/static/js/wallpaper.js
@@ -0,0 +1,16 @@
+function adaptWallpaper() 
+{
+    var wallpaper = document.getElementById('bg');
+    wallpaper.style.width = window.innerWidth + 'px';
+    wallpaper.style.height = window.innerHeight + 'px';
+}
+
+window.onload = function() 
+{
+    adaptWallpaper();
+};
+  
+window.onresize = function() 
+{
+    adaptWallpaper();
+};
