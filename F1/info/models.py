1adfbde41c675c41f8a47bb767fcb72ac2a1e0a7
Anrighe <assoonpc@gmail.com>
2023-05-31 17:57:30 +0200

    rework store + info models

--- F1/info/models.py
+++ F1/info/models.py
@@ -2,11 +2,25 @@ from django.db import models
 from F1.validators import valida_non_negativi
 
 
+class Scuderia(models.Model):
+    nome = models.CharField(max_length=50)
+    descrizione = models.CharField(max_length=50)
+    modello_vettura = models.CharField(max_length=50)
+    immagine_vettura = models.CharField(max_length=100)
+    logo = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Scuderie'
+
+
 class Pilota(models.Model):
     nome = models.CharField(max_length=50)
     eta = models.PositiveSmallIntegerField()
     data = models.DateField()
     foto = models.CharField(max_length=100)
+
+    # Un pilota è ingaggiato da una sola scuderia
+    scuderia = models.ForeignKey(Scuderia, on_delete=models.PROTECT, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'Piloti'
@@ -22,6 +36,7 @@ class Circuito(models.Model):
     giro_veloce = models.DurationField()
     numero_giri = models.PositiveIntegerField()
     inaugurazione = models.DateField()
+    storia = models.CharField(max_length=10000, default='')
 
     class Meta:
         verbose_name_plural = 'Circuiti'
@@ -39,31 +54,32 @@ class Partecipazione(models.Model):
     miglior_tempo = models.DurationField(null=True)  
     data = models.DateField(null=True)
 
+    # Una partecipazione si riferisce a una e una sola sessione
+    sessione = models.ForeignKey(Sessione, on_delete=models.PROTECT, null=True, blank=True)
+
+    # Una partecipazione si riferisce a uno e un solo circuito
+    circuito = models.ForeignKey(Circuito, on_delete=models.PROTECT, null=True, blank=True)
+
+    # Una partecipazione si riferisce a uno e un solo pilota
+    pilota = models.ForeignKey(Pilota, on_delete=models.PROTECT, null=True, blank=True)
+
     class Meta:
         verbose_name_plural = 'Partecipazioni'
 
 
-class Scuderia(models.Model):
-    nome = models.CharField(max_length=50)
-    descrizione = models.CharField(max_length=50)
-    modello_vettura = models.CharField(max_length=50)
-    immagine_vettura = models.CharField(max_length=100)
-    logo = models.CharField(max_length=100)
 
-    class Meta:
-        verbose_name_plural = 'Scuderie'
 
-# Una scuderia ingaggia da 0 a N piloti e un pilota è ingaggiato da una sola scuderia
-Scuderia.ingaggia = models.ForeignKey(Pilota, on_delete=models.PROTECT, null=True, blank=True)
 
-# In una sessione ci sono da 0 a N partecipazioni e una partecipazione si riferisce a una e una sola sessione
-Sessione.vanta = models.ForeignKey(Partecipazione, on_delete=models.CASCADE, null=True, blank=True)
 
-# In un circuito ci sono da 0 a N partecipazioni e una partecipazione si riferisce a uno e un solo circuito
-Sessione.vanta = models.ForeignKey(Partecipazione, on_delete=models.CASCADE, null=True, blank=True)
 
-# Un pilota effettua da 0 a N partecipazioni e una partecipazione si riferisce a uno e un solo pilota
-Pilota.effettua = models.ForeignKey(Partecipazione, on_delete=models.CASCADE, null=True, blank=True)
+
+
+
+
+
+
+
+
 
 
 
--- F1/store/models.py
+++ F1/store/models.py
@@ -1,23 +1,8 @@
 from django.db import models
-from django.contrib.auth.hashers import make_password, check_password
 from F1.validators import valida_non_negativi, valida_iban, valida_carta_credito, valida_cvv
 from django.contrib.auth.models import User
 from info.models import Circuito
 
-
-class Notifica(models.Model):
-    descrizione = models.CharField(max_length=100)
-    data = models.DateField()
-
-    class Meta:
-        verbose_name_plural = 'Notifiche'
-
-
-class Ordine(models.Model):
-    data = models.DateField()
-
-    class Meta:
-        verbose_name_plural = 'Ordini'   
 
 class Gestore_Circuito(models.Model):
     sito_web = models.CharField(max_length=100)
@@ -25,24 +10,14 @@ class Gestore_Circuito(models.Model):
     telefono = models.CharField(max_length=25)
     iban = models.CharField(max_length=34, validators=[valida_iban])
 
+    # Un gestore di un circuito gestisce uno e un solo circuito e un circuito è gestito da uno e un solo gestore
+    gestione_circuito = models.OneToOneField(Circuito, on_delete=models.PROTECT, null=True, blank=True)
+
     # Connette i gestori dei circuiti con la table User
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'Gestori circuiti'
-
-class Biglietto(models.Model):
-    titolo = models.CharField(max_length=50)
-    prezzo = models.FloatField(validators=[valida_non_negativi])
-    numero_posto = models.PositiveIntegerField()
-
-    class Meta:
-        verbose_name_plural = 'Bliglietti'
-
-class Carrello(models.Model):
-
-    class Meta:
-        verbose_name_plural = 'Carrelli'
 
 
 class Utente(models.Model):
@@ -62,34 +37,64 @@ class Utente(models.Model):
     class Meta:
         verbose_name_plural = 'Utenti'
 
-# Un ordine genera da 0 a N notifiche
-# Una notifica è generata al più da un ordine 
-# Se viene cancellato l'ordine, viene cancellata la notifica
-Ordine.genera = models.ForeignKey(Notifica, on_delete=models.CASCADE, null=True, blank=True)
 
-# Un gestore di un circuito riceve da 0 a N ordini
-# Dato un preciso ordine, esso è ricevuto da uno e un solo gestore di un circuito
-# Se viene cancellato un gestore di un circuito, gli ordini ad esso associato non vengono cancellati
-Gestore_Circuito.riceve = models.ForeignKey(Ordine, on_delete=models.PROTECT, null=True, blank=True)
+class Ordine(models.Model):
+    data = models.DateField()
+    
+    # Un ordine si riferisce a uno e un solo gestore di un circuito
+    gestore_circuito = models.ForeignKey(Gestore_Circuito, on_delete=models.PROTECT, null=False, blank=False)
 
-# Un gestore di un circuito gestisce uno e un solo circuito e un circuito è gestito da uno e un solo gestore
-# # Se viene cancellato un circuito, il gestore di un circuito non dev'essere cancellato
-Gestore_Circuito.gestisce = models.OneToOneField(Circuito, on_delete=models.PROTECT, null=True, blank=True)
+    # Un ordine si riferisce a uno e un solo utente
+    utente = models.ForeignKey(Utente, on_delete=models.PROTECT, null=False, blank=False)
 
-# Un ordine contiene da 1 a N biglietti
-# Se un preciso biglietto è stato acquistato è contenuto in al più un ordine
-# Se non è stato acquistato non è contenuto in nessun ordine
-# Se l'ordine viene cancellato, il biglietto non dev'essere cancellato
-Ordine.contiene = models.ForeignKey(Biglietto, on_delete=models.PROTECT, null=True, blank=True)
+    class Meta:
+        verbose_name_plural = 'Ordini'   
 
-# Un carrello può contenere da 0 a N biglietti
-# Un preciso biglietto può essere contenuto da 0 a N carrelli
-# Esempio: più utenti hanno inserito lo stesso biglietto nel proprio carrello
-Carrello.contiene = models.ManyToManyField(Biglietto, null=True, blank=True)
 
-# Un preciso carrello è posseduto da uno e un solo utente
-# In caso di cancellazione dell'utente è necessario cancellare anche il carrello
-Carrello.possiede = models.OneToOneField(Utente, on_delete=models.CASCADE)
+class Notifica(models.Model):
+    descrizione = models.CharField(max_length=100)
+    data = models.DateField()
+
+    # Una notifica si riferisce al più a un ordine
+    ordine = models.ForeignKey(Ordine, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Notifiche'
+
+
+class Carrello(models.Model):
+    # Un preciso carrello è posseduto da uno e un solo utente
+    # In caso di cancellazione dell'utente è necessario cancellare anche il carrello
+    possedimento_carrello = models.OneToOneField(Utente, on_delete=models.CASCADE, default=None)
+
+    class Meta:
+        verbose_name_plural = 'Carrelli'
+
+
+class Biglietto(models.Model):
+    titolo = models.CharField(max_length=50)
+    prezzo = models.FloatField(validators=[valida_non_negativi])
+    numero_posto = models.PositiveIntegerField()
+
+    # Se un preciso biglietto è stato acquistato è contenuto in al più un ordine
+    # Se non è stato acquistato non è contenuto in nessun ordine
+    ordine = models.ForeignKey(Ordine, on_delete=models.PROTECT, null=True, blank=True)
+
+    # Un preciso biglietto può essere contenuto da 0 a N carrelli
+    # Esempio: più utenti hanno inserito lo stesso biglietto nel proprio carrello
+    carrello = models.ManyToManyField(Carrello, blank=True)
+
+    # Un preciso biglietto viene pubblicato nel negozio da uno e un solo gestore di un circuito
+    gestore_circuito = models.ForeignKey(Gestore_Circuito, on_delete=models.PROTECT, null=False, blank=False)
+
+    class Meta:
+        verbose_name_plural = 'Bliglietti'
+
+
+
+
+
+
 
 
 
