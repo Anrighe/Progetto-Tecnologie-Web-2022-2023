8e109a3d0694d23d788db2f6225835da164bfe28
Anrighe <assoonpc@gmail.com>
2023-06-03 09:43:28 +0200

    fixed favicon.ico 404

--- F1/F1/urls.py
+++ F1/F1/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.contrib.auth import views as auth_views
@@ -21,6 +22,7 @@ from django.contrib.auth import views as auth_views
 from .views import prova
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/images/icons/favicon.ico')),
     path('', include('media.urls')),
     path('admin/', admin.site.urls),
     path('prova/', prova, name='prova'),
