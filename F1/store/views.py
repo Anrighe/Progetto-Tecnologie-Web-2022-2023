1646f6ced935dd56dac26548f265cb0841e20386
Anrighe <assoonpc@gmail.com>
2023-06-28 23:40:11 +0200

    User input validation + error handling

--- F1/store/views.py
+++ F1/store/views.py
@@ -4,36 +4,50 @@ from django.views.generic.list import ListView
 from store.models import Utente
 from django.core.files.storage import FileSystemStorage
 import os
+from PIL import Image
+from django.contrib import messages
 
 
 @login_required
 def UserProfile(request):
+    ALLOWED_IMAGE_FORMATS = ['JPEG', 'JPG', 'PNG']
     user = request.user
     
     try:
         utente = Utente.objects.get(user=user)
 
         # Caricamento di una nuova immagine profilo
-        if request.method == 'POST' and request.FILES['profile-image-file']:
-            uploaded_file = request.FILES['profile-image-file']
-
-            username = user.username
+        if request.method == 'POST':
+            uploaded_file = request.FILES.get('profile-image-file')
             
-            path = os.path.join(os.getcwd(), 'static', 'users', username)
+            # Se è stato effettivamente selezionato e caricato un file 
+            if uploaded_file:
+                image = Image.open(uploaded_file)
 
-            # Cancella i file precedentemente presenti nella cartella dell'utente
-            for file_name in os.listdir(path):
-                file_path = os.path.join(path, file_name)
-                if os.path.isfile(file_path):
-                    os.remove(file_path)
+                if image.format in ALLOWED_IMAGE_FORMATS:
 
-            fs = FileSystemStorage(location=path)
+                    username = user.username
+                    
+                    path = os.path.join(os.getcwd(), 'static', 'users', username)
 
-            filename = fs.save(uploaded_file.name, uploaded_file)
-            print("FILENAME", filename)
+                    # Cancella i file precedentemente presenti nella cartella dell'utente
+                    for file_name in os.listdir(path):
+                        file_path = os.path.join(path, file_name)
+                        if os.path.isfile(file_path):
+                            os.remove(file_path)
 
-            utente.immagine_profilo = f'/static/users/{username}/{filename}'
-            utente.save()
+                    fs = FileSystemStorage(location=path)
+
+                    filename = fs.save(uploaded_file.name, uploaded_file)
+                    print("FILENAME", filename)
+
+                    utente.immagine_profilo = f'/static/users/{username}/{filename}'
+                    utente.save()
+                else:
+                    messages.error(request, 'Formato dell\'immagine non valido. Utilizza un file in formato JPEG, JPG, o PNG.')
+            else:
+                messages.error(request, 'Non è stata caricata nessuna immagine.')
+
 
     except Utente.DoesNotExist:
         utente = None
